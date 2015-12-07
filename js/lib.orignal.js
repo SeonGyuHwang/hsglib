2a85d05dd59615fce8b249d2da8a4a4e2fa30550
황선규 <hsg377@gmail.com>
2015-12-07 17:32:37 +0900

    포트 수정

--- js/lib.orignal.js
+++ js/lib.orignal.js
@@ -1,6 +1,6 @@
 (function($){
 	"use strict";
-	var socket = io.connect(location.protocol +"//"+ location.hostname +":8080");
+	var socket = io.connect(location.protocol +"//"+ location.hostname +":"+ location.port);
 	var me = null, pageMove = true, joinCheck = false;
 	window.onunload = function(){ socket.disconnect(); };
 	
